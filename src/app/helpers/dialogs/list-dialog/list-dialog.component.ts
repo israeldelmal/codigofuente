6c6bd4fc197f27fd924c83a1b2f45576baaa9a75
IsraelArzateOrderOS <46330612+IsraelArzateOrderOS@users.noreply.github.com>
2019-10-03 20:03:56 -0600

    Delete Dialog

--- src/app/helpers/dialogs/list-dialog/list-dialog.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'dialog-list',
-    templateUrl: './list-dialog.component.html',
-    styleUrls: [ './list-dialog.component.scss' ]
-})
-export class ListDialogComponent {}
